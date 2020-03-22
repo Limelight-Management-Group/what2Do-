695f94cbbf72465f83101821a617eca4f9559e30
waynebanks <thestorerun@yahoo.com>
2020-03-21 23:13:17 -0700

    finished snake game; now, I will work on the animating the objects that I randomized in the activities array. Time to branch off

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -1,14 +1,74 @@
 console.log(" i am checking in from sketch.js");
 
+let snake;
+var rez = 10;
+var food;
+let w;
+let h;
+
+
+
 function setup(){
-  createCanvas(640,480);
+  createCanvas(450,450);
+  w = floor(width / rez);
+  h = floor(height / rez);
+  frameRate(5); 
+  snake = new Snake();
+  foodLocation();
 }
 
-function draw (){
-  if (mouseIsPressed){
-    fill(0);
-  } else{
-    fill(255);
-  }
-  ellipse(mouseX, mouseY,80,80);
+function foodLocation(){
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+  console.log('this is the food:', food)  
 }
+
+function keyPressed(){
+  if (keyCode === LEFT_ARROW){
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW){
+    snake.setDir(1, 0);
+
+  } else if (keyCode === DOWN_ARROW) {
+    snake.setDir(0, 1);
+
+  } else if (keyCode === UP_ARROW){
+    snake.setDir(0, -1);
+
+  } else if (keyCode === ' '){
+    snake.grow();
+  }
+}
+
+
+function draw(){
+  scale(rez);
+  background(220);
+
+  if(snake.eat(food)){
+    foodLocation();
+  }
+  // snake.eat(food);
+  snake.update();
+  snake.show();
+
+  if (snake.isDead()){
+    print("GameOva");
+    background(255, 0, 0);
+    noLoop();
+  }
+
+  fill(255, 0, 0);
+  noStroke();
+  rect(food.x, food.y, 1, 1);
+}
+
+// function draw (){
+//   if (mouseIsPressed){
+//     fill(0);
+//   } else{
+//     fill(255);
+//   }
+//   ellipse(mouseX, mouseY,80,80);
+// }
--- public/js/snake.js
+++ public/js/snake.js
@@ -2,6 +2,65 @@ console.log("checking in from Snake.js");
 
 class Snake {
   constructor(){
+    this.body = [];
+    this.body[0] = createVector(floor(w/2),floor(h/2));
+    this.xdir = 0;
+    this.ydir = 0;
+    this.size = 1;
+  }
+  setDir(x, y){
+    this.xdir = x;
+    this.ydir = y;
+  }
 
+  update(){
+    var head = this.body[this.body.length-1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+    console.log("this is the head", head)
+    console.log("this is the length of body", this.body.length)
+  }
+
+  grow(){
+    var head = this.body[this.body.length-1].copy();
+    this.size++;
+    this.body.push(head);
+  }
+
+  isDead(){
+    let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if (x > w-1 || x < 0 || y > h-1 || h < 0){
+      return true;
+    }
+
+    for(var i = 0; i < this.body.length-1; i++){
+      var part = this.body[i];
+      if(part.x == x && part.y == y){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  eat(pos){
+    let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if(x == pos.x && y == pos.y){
+      this.grow();
+      return true;
+
+    }
+    return false; 
+  }
+
+  show(){
+    for(var i = 0; i < this.body.length; i++){
+    fill(0);
+    noStroke();
+    rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
   }
 }
